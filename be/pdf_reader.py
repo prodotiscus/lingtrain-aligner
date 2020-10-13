c81000389a5179bf9cfe6817d8da03a398ef5705
Fyodor Sizov <f.sizov@yandex.ru>
2020-10-13 17:35:01 +0300

    flagging and new iterable method

--- be/pdf_reader.py
+++ be/pdf_reader.py
@@ -16,6 +16,7 @@ class PageBuffer:
         self.minimal = page
         self.page = page
         self.pages = {}
+        self.occurrences = {}
 
     def write(self, x):
         if self.page not in self.pages:
@@ -27,8 +28,51 @@ class PageBuffer:
         for n in range(self.minimal, self.page):
             yield n, self.pages[n]
 
+    def by_line(self, rules):
+        for page_index, page_content in self.get_pages():
+            for line_index, line_content in enumerate(page_content.splitlines()):
+                line = FlaggedPageLine(line_content, self, page_index, line_index)
+                for (func, flg) in rules.items():
+                    if func(line):
+                        line.flags = flg
+                yield page_index, line_index, line.text
+
     def create_json(self):
         return json.dumps(self.__dict__, indent=2)
+
+
+class FlaggedPageLine:
+    def __init__(self, linestring, buffer, pin, lin):
+        self.text = linestring
+        self.buffer = buffer
+        self.pin = pin
+        self.lin = lin
+        self._flags = []
+
+    @property
+    def flags(self):
+        return self._flags
+
+    @flags.setter
+    def flags(self, new_flags):
+        for f in set(new_flags):
+            if f not in self._flags:
+                self._flags.append(f)
+                if f not in self.buffer.occurrences:
+                    self.buffer.occurrences[f] = []
+                self.buffer.occurrences[f] = (self.pin, self.lin)
+
+    def __gt__(self, flag):
+        if flag not in self.buffer.occurrences:
+            return False
+        o = self.buffer.occurrences[flag]
+        return o[0] > self.pin or o[1] > self.lin
+
+    def __lt__(self, flag):
+        if flag not in self.buffer.occurrences:
+            return False
+        o = self.buffer.occurrences[flag]
+        return o[0] < self.pin or o[1] < self.lin
 
 
 class PageBufferFromJSON(PageBuffer):
@@ -50,8 +94,3 @@ def put_pdf_into_buffer(file_path):
             buff.page += 1
 
     return buff
-
-
-class ClassifyLine:
-    def __init__(self, content, index):
-        ...

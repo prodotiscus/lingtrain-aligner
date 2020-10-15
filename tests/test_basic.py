8235a43a9bfebd03310a137b58abc6b812385aa7
Fyodor Sizov <f.sizov@yandex.ru>
2020-10-15 13:25:14 +0300

    pdf files are saved into tests/

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,21 @@
 from be.pdf_reader import *
+import os
 import PyPDF2
 
 
+def t(path):
+    return os.path.join("tests", path)
+
+
 def test_main():
-    r = PyPDF2.PdfFileReader("Nabokov-Pale_Fire.pdf")
+    r = PyPDF2.PdfFileReader(t("Nabokov-Pale_Fire.pdf"))
 
     writer = PyPDF2.PdfFileWriter()
 
     for page in range(0, 4):
         writer.addPage(r.getPage(page))
 
-    output_filename = "Nabokov-Pale_Fire-cut-0-4.pdf"
+    output_filename = t("Nabokov-Pale_Fire-cut-0-4.pdf")
 
     with open(output_filename, "wb") as output:
         writer.write(output)
